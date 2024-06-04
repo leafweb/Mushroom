03a3b703db94394dd6801a42aeba15cfc8f4dda1
leafweb <leafweb.io@gmail.com>
2024-06-04 15:23:00 +0330

    update main.js

--- script/main.js
+++ script/main.js
@@ -972,7 +972,7 @@ function Code() {
    CopyBtn();
 }
 
-let M = Mushroom();
+let M = Mushroom({addTo: ':root,:host'});
 let preM = Mushroom({addTo: 'pre,color-picker'});
 M.setDarkmode(Mode());
 preM.setDarkmode(Mode());
